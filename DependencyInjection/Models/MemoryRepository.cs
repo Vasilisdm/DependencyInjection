973bd1d965041da00c9a01395251f70b1606eef4
vasilisdimitriou <vasilisdmr@gmail.com>
2019-09-25 23:03:00 +0300

    Created Memoryrepository, which implements the IRepository interface.

---
+++ DependencyInjection/Models/MemoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DependencyInjection.Models
+{
+    public class MemoryRepository : IRepository
+    {
+        public Product this[string name] => throw new NotImplementedException();
+
+        public IEnumerable<Product> Products => throw new NotImplementedException();
+
+        public void AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
