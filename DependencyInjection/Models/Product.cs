2e9b4490814ad03364d529706372d16b1affcd51
vasilisdimitriou <vasilisdmr@gmail.com>
2019-09-25 22:49:17 +0300

    Added product's properties.

--- DependencyInjection/Models/Product.cs
+++ DependencyInjection/Models/Product.cs
@@ -3,8 +3,7 @@ namespace DependencyInjection.Models
 {
     public class Product
     {
-        public Product()
-        {
-        }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
     }
 }
