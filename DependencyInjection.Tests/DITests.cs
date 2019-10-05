1cc8e126a2c2a32956856dee23f6eb7abcf4b19c
vasilisdimitriou <vasilisdmr@gmail.com>
2019-10-05 16:06:16 +0300

    Created a test that checks if the data passed to the view, are the same that are being shown.

---
+++ DependencyInjection.Tests/DITests.cs
@@ -0,0 +1,31 @@
+﻿using DependencyInjection.Controllers;
+using DependencyInjection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace DependencyInjection.Tests
+{
+    public class DITests
+    {
+        [Fact]
+        public void ControllerTest()
+        {
+            // Arrange
+            var data = new[] { new Product { Name = "Test", Price = 100 } };
+            var mockRepository = new Mock<IRepository>();
+            mockRepository.SetupGet(m => m.Products).Returns(data);
+
+            HomeController controller = new HomeController
+            {
+                Repository = mockRepository.Object
+            };
+
+            // Act
+            ViewResult result = controller.Index();
+
+            // Assert
+            Assert.Equal(data, result.ViewData.Model);
+        } 
+    }
+}
