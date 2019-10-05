2c27d3378f6104a70028d5275eea265706837e65
vasilisdimitriou <vasilisdmr@gmail.com>
2019-10-05 15:46:37 +0300

    Decoupled the Homecontroller and memory repository by adding a property of IRepository.

--- DependencyInjection/Controllers/HomeController.cs
+++ DependencyInjection/Controllers/HomeController.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using DependencyInjection.Models;
 
 namespace DependencyInjection.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index() => View(new MemoryRepository().Products);
+        public IRepository Repository { get; set; } = new MemoryRepository();
+
+        public ViewResult Index() => View(Repository.Products);
     }
 
 }
